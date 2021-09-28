65b9d188dae91d0686f8cf9fb84c2fb70482db59
Line Guld <linekguld@hotmail.com>
2021-09-28 14:44:43 +0200

    bhjg

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -18,9 +18,6 @@ namespace Assignment1
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
